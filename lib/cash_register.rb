8ec1832eee0df2824560ba6ada685a9407f1ca8a
Annia Forney <anniaforney@gmail.com>
2019-06-19 21:35:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,8 @@ class CashRegister
 
   def add_item(title , price , quantity=1)
     self.total += price * quantity
+    self.total = self.total - self.last_transaction
+    
   end
 
   def apply_discount
