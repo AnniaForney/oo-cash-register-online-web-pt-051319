c762f0f7bb50be728a35e13231f5cf6cfd67c459
Annia Forney <anniaforney@gmail.com>
2019-06-19 21:13:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,6 +6,7 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(title , price , quantity=1)
@@ -14,7 +15,7 @@ class CashRegister
 
   def apply_discount
     # total => total - (total x (discount/100))
-    if discount != 0 
+    if discount != 0
       @total = @total - (@total) * (@discount/100.to_f)
     return "After the discount, the total comes to $800."
       else
