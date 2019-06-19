561b1f5ea67dac08ec5e88a033c8c8ba9bbc84ba
Annia Forney <anniaforney@gmail.com>
2019-06-19 21:19:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class CashRegister
 
-  attr_accessor :total, :discount, :items, :apply_discount, :void_last_transaction
+  attr_accessor :total, :discount, :items, :apply_discount, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
