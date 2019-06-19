93d50afcd86e1aa249b64a0805dbbfa7bb496aa1
Annia Forney <anniaforney@gmail.com>
2019-06-19 21:08:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   def apply_discount
     # total => total - (total x (discount/100))
     @total = @total - (@total) * (@discount/100.to_f)
-    
+    puts "After the discount, the total comes to $800."
 
 
   end
